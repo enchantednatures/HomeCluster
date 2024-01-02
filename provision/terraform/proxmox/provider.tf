9a5e12664cf244fd1cef5928f85c41e0895ae448
hcasten <hcasten@outlook.com>
2024-01-01 21:53:26 -0800

    include proxmox provisioning

---
+++ provision/terraform/proxmox/provider.tf
@@ -0,0 +1,51 @@
+# Proxmox Provider
+# ---
+# Initial Provider Configuration for Proxmox
+
+terraform {
+
+  required_version = ">= 0.13.0"
+
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "=0.38.1"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.1"
+    }
+    tailscale = {
+      source  = "tailscale/tailscale"
+      version = "0.13.13"
+    }
+  }
+}
+
+
+
+
+provider "proxmox" {
+  endpoint  = "https://${var.proxmox_host}:8006/api2/json"
+  api_token = var.proxmox_api_token
+  # (Optional) Skip TLS Verification
+  insecure = true
+
+  ssh {
+    agent    = true
+    username = "root"
+  }
+}
+
+provider "tailscale" {
+  api_key = "tskey-api-kT5thp5CNTRL-YZmmhEPxHrHqST1RLCc6sH2vvHG7a48pD"
+  tailnet = "enchantednatures.github"
+}
+
+
+
+
