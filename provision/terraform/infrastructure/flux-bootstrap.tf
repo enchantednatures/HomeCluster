deb4c22b3c300010f24ad4463141b9aae75d640d
hcasten <huntercasten@icloud.com>
2024-07-08 21:29:11 -0600

    remove flux bootstrap

--- provision/terraform/infrastructure/flux-bootstrap.tf
+++ provision/terraform/infrastructure/flux-bootstrap.tf
@@ -1,38 +1,38 @@
-provider "flux" {
-  kubernetes = {
-    config_path = "~/.kube/config"
-  }
-  git = {
-    url = "ssh://git@github.com/${var.github_org}/${var.github_repository}.git"
-    ssh = {
-      username    = "git"
-      private_key = tls_private_key.flux.private_key_pem
-    }
-  }
-}
+# provider "flux" {
+#   kubernetes = {
+#     config_path = "~/.kube/config"
+#   }
+#   git = {
+#     url = "ssh://git@github.com/${var.github_org}/${var.github_repository}.git"
+#     ssh = {
+#       username    = "git"
+#       private_key = tls_private_key.flux.private_key_pem
+#     }
+#   }
+# }
 
-provider "github" {
-  owner = var.github_org
-  token = var.github_token
-}
+# provider "github" {
+#   owner = var.github_org
+#   token = var.github_token
+# }
 
 
 
-resource "tls_private_key" "flux" {
-  algorithm   = "ECDSA"
-  ecdsa_curve = "P256"
-}
+# resource "tls_private_key" "flux" {
+#   algorithm   = "ECDSA"
+#   ecdsa_curve = "P256"
+# }
 
-resource "github_repository_deploy_key" "this" {
-  title      = "Flux"
-  repository = var.github_repository
-  key        = tls_private_key.flux.public_key_openssh
-  read_only  = "false"
-}
+# resource "github_repository_deploy_key" "this" {
+#   title      = "Flux"
+#   repository = var.github_repository
+#   key        = tls_private_key.flux.public_key_openssh
+#   read_only  = "false"
+# }
 
-resource "flux_bootstrap_git" "this" {
-  depends_on = [github_repository_deploy_key.this]
+# resource "flux_bootstrap_git" "this" {
+#   depends_on = [github_repository_deploy_key.this]
 
-  embedded_manifests = true
-  path               = "kubernetes/flux"
-}
+#   embedded_manifests = true
+#   path               = "kubernetes/flux"
+# }
