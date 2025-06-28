c43a485c6d07a4ac24c94eaf00105cce324253e0
Hunter Casten <huntercasten@icloud.com>
2025-06-27 19:33:48 -0700

    flux terraform

--- provision/terraform/proxmox/variables.tf
+++ provision/terraform/proxmox/variables.tf
@@ -13,3 +13,20 @@ variable "proxmox" {
 
 variable "aws_access_key" { type = string }
 variable "aws_secret_key" { type = string }
+
+# Flux GitOps variables
+variable "github_owner" {
+  description = "GitHub repository owner"
+  type        = string
+}
+
+variable "github_repository" {
+  description = "GitHub repository name"
+  type        = string
+}
+
+variable "age_private_key" {
+  description = "Age private key for SOPS decryption"
+  type        = string
+  sensitive   = true
+}
