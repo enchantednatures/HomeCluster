e976d02f93214b710358ae83825174c6b90c214e
Hunter Casten <huntercasten@icloud.com>
2025-07-20 00:36:59 -0700

    moved to infra

--- provision/terraform/proxmox/bootstrap/volumes/persistent-volume/providers.tf
+++
@@ -1,9 +0,0 @@
-# tofu/bootstrap/volumes/persistent-volume/providers.tf
-terraform {
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = ">= 2.31.0"
-    }
-  }
-}
--- provision/terraform/proxmox/bootstrap/volumes/proxmox-volume/providers.tf
+++
@@ -1,9 +0,0 @@
-# tofu/bootstrap/volumes/proxmox-volumes/providers.tf
-terraform {
-  required_providers {
-    restapi = {
-      source  = "Mastercard/restapi"
-      version = ">= 1.19.1"
-    }
-  }
-}
